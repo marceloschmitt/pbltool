6ecaba7d6b3eeb7f48cbbf32175a4575027abaa5
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2015-04-13 11:46:01 -0300

    Codechecker fixes

--- authorization.php
+++ authorization.php
@@ -15,50 +15,44 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 
-// ensure we have a valid courseid and can load the associated course object
+// Ensure we have a valid courseid and can load the associated course object.
 
-if (! $course = $DB->get_record('course', array('id' => $courseid)) ) {
-
-	print_error(get_string('invalidcourse', 'block_pbltool'));
+if (!($course = $DB->get_record('course', array('id' => $courseid)))) {
+    print_error(get_string('invalidcourse', 'block_pbltool'));
 }
 
-// ensure the user has access to this course
-
+// Ensure the user has access to this course
 require_login($course);
 
-// ensure the block exits
-if (! $block = $DB->get_record("block_instances", array("id" => $blockid))) {
-
+// Ensure the block exits.
+if (!($block = $DB->get_record("block_instances", array("id" => $blockid)))) {
     error("Block ID was incorrect or no longer exists",$CFG->wwwroot."/course/view.php?id=$courseid");
 }
 
-// ensure the block belongs to the course
-//if ($block->pageid != $courseid) {
-//    error("Block ID does not belong to course",$CFG->wwwroot."/course/view.php?id=$courseid");
-//}
+// Ensure the block belongs to the course.
+/*if ($block->pageid != $courseid) {
+    error("Block ID does not belong to course",$CFG->wwwroot."/course/view.php?id=$courseid");
+}
+*/
 
 $context = context_course::instance($courseid);
 
-// ensure the user has appropriate permissions to access this area
+// Ensure the user has appropriate permissions to access this area.
 require_capability('block/pbltool:viewpages', $context);
 
-// ensure the user belongs to the group if not admin and if not entering block
-if($groupid && !has_capability('block/pbltool:managepages', $context)) {
-
-	$teacher = false;
-    	$aux = groups_get_all_groups($courseid,$USER->id);
-
-    	if(!$aux) //If no group is defined
-
-    	{   error('No group definition',$CFG->wwwroot."/course/view.php?id=$courseid");
-    	}
-    	foreach($aux as $group);
-
-    	if($group->id != $groupid) {
-
-       		error('You are trying to access a group that you are not authorized',$CFG->wwwroot."/course/view.php?id=$courseid");
-
-    	}	
+// Ensure the user belongs to the group if not admin and if not entering block
+if ($groupid && !has_capability('block/pbltool:managepages', $context)) {
+    $teacher = false;
+    $aux = groups_get_all_groups($courseid, $USER->id);
+    if (!$aux) { //If no group is defined.
+        error('No group definition',$CFG->wwwroot."/course/view.php?id=$courseid");
+    }
+    foreach ($aux as $group);
+   	if($group->id != $groupid) {
+        error('You are trying to access a group that you are not authorized',
+            $CFG->wwwroot."/course/view.php?id=$courseid");
+    }	
 }
-else
+else {
 	$teacher = true;
+}
